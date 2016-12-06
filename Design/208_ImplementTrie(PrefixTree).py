8aab6a12b75609026ff93a28da151825e733ad3b
GQQ-Surface <jilianggqq@163.com>
2016-12-05 18:25:46 -0800

    implement prefix tree in python

--- Design/208_ImplementTrie(PrefixTree).py
+++ Design/208_ImplementTrie(PrefixTree).py
@@ -4,8 +4,8 @@ class TrieNode(object):
         Initialize your data structure here.
         """
         self.children = [None]*26
-		self.isword = False
-		self.char = None
+        self.isword = False
+        self.char = None
 
 class Trie(object):
 
@@ -19,11 +19,12 @@ class Trie(object):
         :rtype: void
         """
         wd = self.root
-		for c in word:
-			if wd.children[ord(c)-ord(a)] is None:
-				wd.children[ord(c)-ord(a)] = TrieNode()
-				wd.children[ord(c)-ord(a)].char = c
-		wd.wd.children[ord(c)-ord(a)].isword = True
+        for c in word:
+            if wd.children[ord(c)-ord("a")] is None:
+                wd.children[ord(c)-ord("a")] = TrieNode()
+                wd.children[ord(c)-ord("a")].char = c
+            wd = wd.children[ord(c)-ord("a")]
+        wd.isword = True
 
     def search(self, word):
         """
@@ -31,7 +32,13 @@ class Trie(object):
         :type word: str
         :rtype: bool
         """
-        
+        wd = self.root
+        for c in word:
+            cld = wd.children[ord(c)-ord("a")]
+            if cld is None:
+                return False
+            wd = cld
+        return wd.isword
 
     def startsWith(self, prefix):
         """
@@ -40,7 +47,13 @@ class Trie(object):
         :type prefix: str
         :rtype: bool
         """
-        
+        wd = self.root
+        for c in prefix:
+            cld = wd.children[ord(c)-ord("a")]
+            if cld is None:
+                return False
+            wd = cld
+        return True
 
 # Your Trie object will be instantiated and called as such:
 # trie = Trie()
