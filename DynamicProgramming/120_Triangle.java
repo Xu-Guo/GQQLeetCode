1dded0ad6c05a26cd6758161eb7c9ee694050ebb
Qiqiang Guan <qiqiang.guan@outlook.com>
2016-12-12 18:58:54 -0800

    finished 120 Triangle.

---
+++ DynamicProgramming/120_Triangle.java
@@ -0,0 +1,19 @@
+/*
+The idea is simple.
+
+1. Go from bottom to top.
+
+2. We start form the row above the bottom row [size()-2].
+
+3. Each number add the smaller number of two numbers that below it.
+
+4. And finally we get to the top we the smallest sum.
+*/
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for(int i = triangle.size() - 2; i >= 0; i--)
+            for(int j = 0; j <= i; j++)
+                triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
+        return triangle.get(0).get(0);
+    }
+}
---
+++ DynamicProgramming/120_Triangle.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        t = triangle
+        if len(t) == 1:
+            return t[0][0]
+        ret = [0]*len(t)
+        ret[0] = t[0][0]
+        for i in range(1, len(t)):
+            for j in range(0, i+1):
+                if j == 0:
+                    old_v = ret[j]
+                    ret[j] += t[i][j]
+                elif j == i:
+                    ret[j] = old_v + t[i][j]
+                else:
+                    val = min(old_v + t[i][j], ret[j] + t[i][j])
+                    old_v = ret[j]
+                    ret[j] = val
+        return min(ret)
