c3e2833963571383ed481deeac1931361c8e96d2
Qiqiang Guan <qiqiang.guan@outlook.com>
2017-01-20 17:12:41 -0800

    finished 132 and 322

--- DynamicProgramming/132_PalindromePartitioningII.py
+++ DynamicProgramming/132_PalindromePartitioningII.py
@@ -1,28 +1,28 @@
 class Solution(object):
-# error test case : abbab
+    # error test case : abbab
+
     def minCut(self, s):
         """
         :type s: str
         :rtype: int
         """
-        if len(s) <= 1:
-            return 0
-        res = [0] * len(s)
-        for i in range(1, len(s)):
-            for j in range(0, i + 1):
-                if self.is_palindrome(s[j:i + 1]):
-                    if j == 0:
-                        res[i] = 0
+        # b[i], the minCut [i:n-1]. the result is b[0]
+        # pay[i][j]. to charge whether [i:j] is Pal.
+        # if pay[i][j] is true:
+        #   if j=n-1, b[i] = 0
+        #   else. b[i] = min(b[i], 1+b[j+1])
+        n = len(s)
+        pal = [[False for i in range(n)] for j in range(n)]
+        b = [0 for i in range(n)]
+        for i in range(n - 1, -1, -1):
+            b[i] = n - 1 - i
+            for j in range(i, n):
+                # use this if to check [i:j] is palindrome
+                # print i, j
+                if s[i] == s[j] and (j - i < 2 or pal[i + 1][j - 1]):
+                    pal[i][j] = True
+                    if j == n - 1:
+                        b[i] = 0
                     else:
-                        res[i] = res[j-1] + 1
-                    break
-        print res
-        return res[-1]
-
-    def is_palindrome(self, s):
-        i, j = 0, len(s) - 1
-        while i <= j:
-            if s[i] != s[j]:
-                return False
-            i, j = i + 1, j - 1
-        return True
+                        b[i] = min(b[i], 1 + b[j + 1])
+        return b[0]
