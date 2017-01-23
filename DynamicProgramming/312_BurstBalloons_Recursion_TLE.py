36c7d8c29cf9a4e324ecf577311233cf1cd5125e
GQQ-Surface <jilianggqq@163.com>
2017-01-22 22:06:25 -0800

    use cache for 312, it is still TLE

--- DynamicProgramming/312_BurstBalloons_Recursion_TLE.py
+++ DynamicProgramming/312_BurstBalloons_Recursion_TLE.py
@@ -5,12 +5,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        return self.maxC(nums, {})
+
+    def maxC(self, nums, cache):
         mval = 0
         n = len(nums)
         if n == 0:
             return 0
         if n == 1:
             return nums[0]
+        key = tuple(nums)
+        if cache.has_key(key):
+            return cache.get(key)
+
         for i in range(0, n):
             left = 1 if i == 0 else nums[i - 1]
             right = 1 if i == n - 1 else nums[i + 1]
@@ -20,9 +27,11 @@ class Solution(object):
                 subarr = nums[0:n - 1]
             else:
                 subarr = nums[0:i] + nums[i + 1:n]
-            mval = max(mval, left * nums[i] * right + self.maxCoins(subarr))
+            mval = max(mval, left * nums[i] * right + self.maxC(subarr, cache))
+        cache[key] = mval
         return mval
+
 
 if __name__ == '__main__':
     s = Solution()
-    print s.maxCoins([35,16,83,87,84,59,48,41,20,54])
+    print s.maxCoins([2, 4, 8, 4, 0, 7, 8, 9, 1, 2, 4, 7, 1, 7, 3, 6])
