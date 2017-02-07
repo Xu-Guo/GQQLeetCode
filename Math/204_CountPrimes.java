30240e912d88920e2913ce4d832ed2f6cc31e759
peter BigPc ubuntu <peter_BigPc_ubuntu@itu.edu>
2017-02-06 16:45:50 -0800

    finished 138 202 204

---
+++ Math/204_CountPrimes.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int countPrimes(int n) {
+        if (n < 2) {
+            return 0;
+        }
+        boolean[] tbl = new boolean[n];
+        tbl[0] = tbl[1] = true;
+        for (int j = 3; j < n; j++) {
+            if (j % 2 == 0) tbl[j] = true;
+        }
+
+        for (int i=3; i*i < n; i++) {
+            if(!tbl[i]) {
+                for (int j=2; j*i<n; j++) {
+                    tbl[j*i] = true;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i=2; i<n; i++) {
+            if(!tbl[i]) cnt++;
+        }
+        return cnt;
+    }
+}
