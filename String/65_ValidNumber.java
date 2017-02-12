448d77b785423b7053257de287ac1e0f16932530
GQQ-Surface <jilianggqq@163.com>
2017-02-11 23:44:50 -0800

    finished 65

--- String/65_ValidNumber.java
+++ String/65_ValidNumber.java
@@ -9,19 +9,34 @@ public class Solution {
         }
         if (s.length() == 0) return false;
 
-        int eIndex = -1;
-        int dotIndex = -1;
+        int hasdot = -1;
         int n = s.length();
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
             if (c == 'e') {
-                if (i == 0 || i == n - 1 || eIndex != -1) return false;
-                else if (dotIndex == i - 1) return false;
-                else eIndex = i;
+                if (i == 0 || i == n - 1) return false;
+                else if (hasdot == 0 && i == 1) return false;
+                return isAfterENumber(s.substring(i + 1));
             } else if (c == '.') {
-                if (i == 0 || i == n - 1 || dotIndex != -1 || eIndex != -1) return false;
-                else dotIndex = i;
+                if (hasdot != -1 || n == 1) return false;
+                else hasdot = i;
             } else if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isAfterENumber(String s) {
+        System.out.println(s);
+        if (s.length() == 0) return false;
+        if (s.startsWith("+") || s.startsWith("-")) {
+            s = s.substring(1);
+        }
+        if (s.length() == 0) return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c < '0' || c > '9') {
                 return false;
             }
         }
