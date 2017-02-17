9c8e2a69006fdd1a231abdc2af921d58c2920a0b
Qiqiang Guan <qiqiang.guan@outlook.com>
2017-02-17 13:50:43 -0800

    finised 313, add 264

--- Math/264_UglyNumberII.py
+++ Math/264_UglyNumberII.py
@@ -31,3 +31,25 @@ class Solution(object):
                 pos[2] += 1
             start += 1
         return res[-1]
+
+# This is the second loop.
+# obviously second loop is more concise.
+class Solution(object):
+
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        idx = [0, 0, 0]
+        ret = [0] * n
+        ret[0] = 1
+        for i in range(1, n):
+            ret[i] = min(2 * ret[idx[0]], 3 * ret[idx[1]], 5 * ret[idx[2]])
+            if ret[i] == 2 * ret[idx[0]]:
+                idx[0] += 1
+            if ret[i] == 3 * ret[idx[1]]:
+                idx[1] += 1
+            if ret[i] == 5 * ret[idx[2]]:
+                idx[2] += 1
+        return ret[n - 1]
