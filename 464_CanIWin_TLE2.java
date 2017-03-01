75836618ff3c35ee5ece7ab769793a39993f7ba8
GQQ-Surface <jilianggqq@163.com>
2017-02-28 22:34:02 -0800

    464 tle

--- 464_CanIWin_TLE2.java
+++ 464_CanIWin_TLE2.java
@@ -4,25 +4,25 @@ public class Solution {
         for (int i=1; i<=maxChoosableInteger; i++) {
             set.add(i);
         }
-        return helper(set, desiredTotal);
+        return helper(set, new HashSet<Integer>(), desiredTotal);
     }
 
-    public boolean helper(Set<Integer> set, int desiredTotal) {
+    public boolean helper(Set<Integer> set, Set<Integer> excluded, int desiredTotal) {
         int sum = 0;
         // set.forEach(x -> sum += x);
         for (Integer num : set) {
+            if (excluded.contains(num)) continue;
+            if (num >= desiredTotal) return true;
             sum += num;
         }
         if (sum < desiredTotal) return false;
         for (Integer num : set) {
-            if (num >= desiredTotal) return true;
-        }
-        Set<Integer> tmpSet = new HashSet<>(set);
-        for (Integer num : set) {
-            tmpSet.remove(num);
-            boolean r = helper(tmpSet, desiredTotal - num);
+            // System.out.println(num);
+            if (excluded.contains(num)) continue;
+            excluded.add(num);
+            boolean r = helper(set, excluded, desiredTotal - num);
+            excluded.remove(num);
             if (!r) return true;
-            tmpSet.add(num);
         }
         return false;
     }
