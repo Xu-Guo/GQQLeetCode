f5d11c7c20e01282a273f0b2e1566dbf2f01c8dc
GQQ-Surface <jilianggqq@163.com>
2017-02-28 17:20:53 -0800

    finished 297

--- 297_SerializeandDeserializeBinaryTree.java
+++ 297_SerializeandDeserializeBinaryTree.java
@@ -11,12 +11,49 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        
+        if (root == null) return "";
+        List<String> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (null == node) list.add("null");
+            else list.add(String.valueOf(node.val));
+            if (node != null) {
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+
+        while(list.size() > 0 && list.get(list.size() - 1).equals("null")) {
+            list.remove(list.size() - 1);
+        }
+        return String.join(",", list);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        
+        if(data.equals("")) return null;
+        String[] nodes = data.split(",");
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
+        queue.offer(root);
+        int i = 1;
+        while (i < nodes.length) {
+            TreeNode nd = queue.poll();
+            String strLeft = nodes[i++];
+            if (!strLeft.equals("null")) {
+                nd.left = new TreeNode(Integer.parseInt(strLeft));
+                queue.offer(nd.left);
+            }
+            if (i == nodes.length) break;
+            String strRight = nodes[i++];
+            if (!strRight.equals("null")) {
+                nd.right = new TreeNode(Integer.parseInt(strRight));
+                queue.offer(nd.right);
+            }
+        }
+        return root;
     }
 }
 
