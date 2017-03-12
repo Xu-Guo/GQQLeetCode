cf2b433b1138c4fdac0a84107a9b80d8391eb26e
GQQ-Surface <jilianggqq@163.com>
2017-03-11 17:36:30 -0800

    finished 133

--- 133_CloneGraph.java
+++ 133_CloneGraph.java
@@ -8,9 +8,26 @@
  */
 public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        return deepCopy(node, new HashMap<Integer, UndirectedGraphNode>());
+    }
+
+    public UndirectedGraphNode deepCopy(UndirectedGraphNode node, Map<Integer, UndirectedGraphNode> map) {
         if (node == null) return null;
         UndirectedGraphNode newnode = new UndirectedGraphNode(node.label);
         // List<UndirectedGraphNode> neighbors = node.neighbors;
-        new.neighbors = new ArrayList<>(node.neighbors);
+        // new.neighbors = new ArrayList<>(node.neighbors);
+        for (UndirectedGraphNode nb : node.neighbors) {
+            if (nb.label == newnode.label) newnode.neighbors.add(newnode);
+            else {
+                // newnode.neighbors.add(cloneGraph(nb));
+                if (map.containsKey(nb.label)) {
+                    newnode.neighbors.add(map.get(nb.label));
+                } else {
+                    newnode.neighbors.add(deepCopy(nb, map));
+                }
+            }
+        }
+        map.put(newnode.label, newnode);
+        return newnode;
     }
 }
