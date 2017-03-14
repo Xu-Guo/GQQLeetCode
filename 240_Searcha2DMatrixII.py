4e108c5595a2328585ce1c57ddc3f95501d4bd8c
GQQ-Surface <jilianggqq@163.com>
2017-03-14 14:54:43 -0700

    240

--- 240_Searcha2DMatrixII.py
+++ 240_Searcha2DMatrixII.py
@@ -11,8 +11,13 @@ class Solution(object):
             return False
         n = len(matrix[0])
         int low1, high1, low2, high2 = 0, 0, m - 1, n - 1
+        return self.doSearch(low1, high1, low2, high2, matrix, m, n)
 
-    def doSearch(low1, high1, low2, high2, matrix, m, n):
+    def doSearch(self, low1, high1, low2, high2, matrix, m, n):
+        if low1 < 0 or high1 >= m or low1 > high1:
+            return False
+        if low2 < 0 or high2 >= n or low2 > high2:
+            return False
         if low1 == high1 and low2 == high2:
             return True matrix[low1][low2] == target else False
         elif low1 == high1:
@@ -39,4 +44,13 @@ class Solution(object):
         mid2 = low2 + (high2 - low2) / 2
         if matrix[mid1][mid2] == target:
             return True
-        elif matrix[mid1][mid2] == target:
+        elif matrix[mid1][mid2] < target:
+            test1 = self.doSearch(mid1 + 1, high1, low2, mid2)
+            test2 = self.doSearch(low1, mid1, mid2 + 1, high2)
+            test3 = self.doSearch(mid1 + 1, high1, mid2 + 1, high2)
+            return test1 or test2 or test3
+        else:
+            test1 = self.doSearch(mid1, high1, low2, mid2 - 1)
+            test2 = self.doSearch(low1, mid1 - 1, mid2, high2)
+            test3 = self.doSearch(low1, mid1 - 1, low2, mid2 - 1)
+            return test1 or test2 or test3
