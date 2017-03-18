2a8047e622af7c88a754b28d997374e66ed7682c
GQQ-Surface <jilianggqq@163.com>
2017-03-18 02:15:32 -0700

    25

--- 25_ReverseNodesink-Group.java
+++ 25_ReverseNodesink-Group.java
@@ -9,9 +9,37 @@
 public class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         if (head == null) return head;
-        ListNode pre = head, after = pre.next;
-        for (int i = 1; i < k; i++) {
-            
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode tail = head;
+        boolean needReverse = true;
+        for (int i = 0; i < k; i++) {
+            if (tail == null) {
+                    needReverse = false;
+                    break;
+            }
+            tail = tail.next;
         }
+        while (needReverse) {
+            ListNode p = pre.next;
+            ListNode q = p.next;
+            while (q != tail) {
+                p.next = q.next;
+                q.next = pre.next;
+                pre.next = q;
+                q = p.next;
+            }
+
+            pre = p;
+            for (int i = 0; i < k; i++) {
+                if (tail == null) {
+                    needReverse = false;
+                    break;
+                }
+                tail = tail.next;
+            }
+        }
+        return dummy.next;
     }
 }
